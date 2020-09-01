4f3752f6a99eb8fcd43755648e6fa9b778c16a3f
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-09-01 16:09:55 -0300

    add: route and page Initial

---
+++ frontend/src/pages/Initial/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Initial: React.FC = () => <h1>Initial</h1>;
+
+export default Initial;
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Initial from '../pages/Initial';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route path="/" exact />
+    <Route path="/" exact component={Initial} />
+  </Switch>
+);
