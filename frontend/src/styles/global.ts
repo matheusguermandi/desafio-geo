5a7f1273ebad8adc4a79612d455c7610e91d8cc5
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-09-01 16:24:22 -0300

    add: style global

---
+++ frontend/src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+  body {
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 16px Roboto, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+  #root {
+    max-width: 1300px;
+    margin: 0 auto;
+    padding: 60px 20px;
+  }
+`;
