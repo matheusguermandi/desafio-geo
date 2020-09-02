f83ab12d68ed75b4c7733c80376ed1a0e2852d5e
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-09-02 17:25:56 -0300

    add: Eslint and Prettier

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -3,5 +3,5 @@ import express from 'express';
 const app = express();
 
 app.listen(3333, () => {
-    console.log('Server started on PORT 3333');
-}); 
+  console.log('Server started on PORT 3333');
+});
