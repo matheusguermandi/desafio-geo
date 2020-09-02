fd16a7a429e06c9a47cbee6e170fc782587e29b3
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-09-02 19:18:21 -0300

    add: error handling

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -1,7 +1,27 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import AppError from '../../errors/error';
 import '../mongoose';
 
 const app = express();
+
+app.use(
+  (error: Error, request: Request, response: Response, _: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+
+    console.log(error);
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
+);
 
 app.listen(3333, () => {
   console.log('Server started on PORT 3333');
