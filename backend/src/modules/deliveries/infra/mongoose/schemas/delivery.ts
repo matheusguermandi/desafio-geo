9c58d34da5a2e15119bca35e0f216ba80f595324
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-09-02 19:12:59 -0300

    add: Schema Deliveries

---
+++ backend/src/modules/deliveries/infra/mongoose/schemas/delivery.ts
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+const deliveriesShema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  weight: {
+    type: String,
+    required: true,
+  },
+  num: {
+    type: Number,
+  },
+  neighborhood: {
+    type: String,
+  },
+  complement: {
+    type: String,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: Number,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Delivery = mongoose.model('Deliveries', deliveriesShema);
+
+export default Delivery;
--- backend/src/shared/infra/mongoose/index.ts
+++ backend/src/shared/infra/mongoose/index.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 try {
   mongoose.connect('mongodb://localhost:27017/desafiogeo', {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   });
   mongoose.Promise = global.Promise;
 } catch (error) {
