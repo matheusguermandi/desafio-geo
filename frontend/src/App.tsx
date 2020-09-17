2afc9bf64128467f63ed625bd9bd3e3347ae401c
Matheus Guermandi <matheus_guermandi@hotmail.com>
2020-09-17 18:53:36 -0300

    refactor: app aplication

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import Routes from './routes';
+
 import GlobalStyle from './styles/global';
+
+import Routes from './routes';
+
+import AppProvider from './hooks/index';
 
 const App: React.FC = () => (
   <>
     <BrowserRouter>
-      <Routes />
+      <AppProvider>
+        <Routes />
+      </AppProvider>
     </BrowserRouter>
+
     <GlobalStyle />
   </>
 );
